113d8745d590b17185a1d57045e5dc25a9a214de
na07 <nkef2698@gmail.com>
2025-06-24 19:03:45 +0300

    reg and log page +

---
+++ authenticator/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    STATUS_CHOICES = (
+        ("ONLINE", "Online"),
+        ("OFFLINE", "Offline"),
+        ("IDLE", "Idle"),
+    )
+    owner = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    avatar = models.ImageField()
+    age = models.PositiveIntegerField()
+    bio = models.CharField(max_length=150)
+    status = models.CharField(max_length=9, choices=STATUS_CHOICES, default="OFFLINE")
+    def __str__ (self):
+        return f"Профиль {self.owner.username}"
+
---
+++ authenticator/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import RegisterForm
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            messages.success(request, 'Регистрация прошла успешно!')
+            return redirect('/home_page/')  # замените на вашу страницу входа
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Вы вошли в систему!')
+                return redirect('/home_page/') #ну сделал так
+            else:
+                messages.error(request, 'Неверное имя пользователя или пароль.')
+    else:
+        form = LoginForm()
+    return render(request, 'log.html', {'form': form})
