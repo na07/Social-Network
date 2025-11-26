6702addf9603bbd87576e54638cbb4030b5b0c39
na07 <nkef2698@gmail.com>
2025-11-26 17:32:19 +0300

    last repair

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@xu+ovy)*1+((!^cwy(sef%5%_zfhdzte^uf==dlwp=36686g=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
